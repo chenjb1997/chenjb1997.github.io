920e45e3682d1236d0ef3a011758e4939cc248bd
Duplicate4 <lyuzhi.pan@gmail.com>
2025-04-14 20:48:07 +0800

    refactor: restructuring of the catalog

---
+++ src/Contact.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { FaGithub, FaEnvelope, FaBook, FaFilePdf } from 'react-icons/fa';
+
+const Contact = () => {
+    return (
+        <div style={styles.container}>
+            <h1 style={styles.heading}>Contact</h1>
+            <p style={styles.text}>
+                If you have any questions, would like to discuss my research, or are interested in collaboration opportunities, please don't hesitate to reach out. Below, you can find the best ways to contact me.
+            </p>
+            {/* <h2 style={styles.subHeading}>Contact Information</h2> */}
+            <div style={styles.iconLinks}>
+                <a href="mailto:chenjb1997@gmail.com" style={styles.iconLink}>
+                    <FaEnvelope style={styles.icon} /> Email
+                </a>
+                <a href="https://scholar.google.com/citations?user=UQfmkJUAAAAJ" style={styles.iconLink}>
+                    <FaBook style={styles.icon} /> Google Scholar
+                </a>
+                <a href="/CV.pdf" style={styles.iconLink}>
+                    <FaFilePdf style={styles.icon} /> Download CV
+                </a>
+                <a href="https://github.com/chenjb1997" style={styles.iconLink}>
+                    <FaGithub style={styles.icon} /> GitHub
+                </a>
+            </div>
+        </div>
+    );
+};
+
+const styles = {
+    container: {
+        padding: '20px',
+        fontFamily: '"Helvetica Neue", Helvetica, Arial, sans-serif',
+        maxWidth: '800px',
+        margin: 'auto'
+    },
+    heading: {
+        fontSize: '30px',
+        fontWeight: 'normal',
+        color: '#333'
+    },
+    subHeading: {
+        fontSize: '20px',
+        fontWeight: 'normal',
+        fontStyle: 'italic',
+        color: '#333'
+    },
+    text: {
+        color: '#666',
+        fontSize: '16px',
+        lineHeight: '1.6',
+    },
+    list: {
+        listStyleType: 'none',
+        paddingLeft: '0',
+        lineHeight: '2',
+        marginBottom: '20px'
+    },
+    link: {
+        color: '#0066cc',
+        textDecoration: 'none',
+    },
+    iconLinks: {
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        marginTop: '20px'
+    },
+    iconLink: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        textDecoration: 'none',
+        color: '#666',
+        margin: '10px',
+        textAlign: 'center'
+    },
+    icon: {
+        marginBottom: '5px'
+    }
+};
+
+export default Contact;
---
+++ src/Navbar.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+
+    const [hoverIndex, setHoverIndex] = useState(null);
+
+    const isActiveStyle = (isActive) => ({
+        color: isActive ? '#333' : '#666',
+        textDecoration: 'none',
+        fontSize: '1rem',
+        fontWeight: 'bold',
+        borderBottom: isActive ? '3px solid #333' : 'none',
+        paddingBottom: '2px',
+        transition: 'all 0.1s ease'
+    });
+
+    return (
+        <nav style={styles.navbar}>
+            <ul style={styles.navList}>
+                {['Home', 'Competitive Programming', 'Contact'].map((text, index) => {
+                    const path = text === 'Home' ? '/' : `/${text.toLowerCase().replace(/\s+/g, '-')}`;
+                    return (
+                        <li
+                            key={text}
+                            style={styles.navItem}
+                            onMouseEnter={() => setHoverIndex(index)}
+                            onMouseLeave={() => setHoverIndex(null)}
+                        >
+                            <NavLink
+                                to={path}
+                                style={({ isActive }) => isActiveStyle(isActive)}
+                                onMouseEnter={() => setHoverIndex(index)}
+                                onMouseLeave={() => setHoverIndex(null)}
+                            >
+                                {text}
+                            </NavLink>
+                        </li>
+                    );
+                })}
+            </ul>
+        </nav>
+    );
+};
+
+const styles = {
+    navbar: {
+        backgroundColor: 'white',
+        minHeight: '60px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '0 20px',
+        boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
+    },
+    navList: {
+        listStyle: 'none',
+        display: 'flex',
+        margin: 0,
+        padding: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+    },
+    navItem: {
+        margin: '0 20px',
+    }
+};
+
+export default Navbar;
